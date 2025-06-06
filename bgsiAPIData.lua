60e9df4132f3c54e97de70bf2f0179662f6dfb4d
gotham10 <notagainbruh1236@outlook.com>
2025-06-06 01:24:36 -0500

    Update bgsiAPIData.lua

--- bgsiAPIData.lua
+++ bgsiAPIData.lua
@@ -1,4 +1,12 @@
-local d = game:HttpGet(_G.link)
-local j = d:match("<pre>(.-)</pre>")
-j = j:gsub("&quot;", "\"")
-setclipboard(j)
+local s, r = pcall(function() return game:HttpGet(_G.link) end)
+if s then
+	local p = r:match("<pre>(.-)</pre>")
+	if p then
+		p = p:gsub("&quot;", "\"")
+		setclipboard(p)
+	else
+		setclipboard(r)
+	end
+else
+	setclipboard(tostring(r))
+end
