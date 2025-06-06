6ed0ccad446a00303030dcd8734e25301ecac894
gotham10 <notagainbruh1236@outlook.com>
2025-06-06 01:40:25 -0500

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,11 @@
+--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+_G.link = "https://bgsi-kyc3.onrender.com/items/dementor.png"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gotham10/test/main/bgsiAPIData.lua"))()
+--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+_G.link = {
+	"https://bgsi-kyc3.onrender.com/api/items/dementor",
+	"https://bgsi-kyc3.onrender.com/items/dementor.png",
+	"https://bgsi-kyc3.onrender.com/api/items/silly-doggy"
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gotham10/test/main/bgsiAPIData.lua"))()
+--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
